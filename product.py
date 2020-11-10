207824d25826785d0c4098896a6f2b3933f20742
Jtwopen <jtwopen@gmail.com>
2020-11-10 22:00:26 +0800

    帳單1

--- product.py
+++ product.py
@@ -4,10 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
-	p = []
-	p.append(name)
-	p.append(price)
-	products.append(p)
+	products.append([name,price])
 print(products)
 
 for p in products:
