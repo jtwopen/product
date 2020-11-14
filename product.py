fb1f71127579a7fcc83f7eafe4b4894a1763d72c
Jtwopen <jtwopen@gmail.com>
2020-11-14 10:53:22 +0800

    sss

--- product.py
+++ product.py
@@ -1,13 +1,28 @@
 products = []
+
+with open('menu.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品名稱,價錢' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
-	products.append([name,price])
-print(products)
+	products.append([name, price])
 
+#印出所有購買紀錄
 for p in products:
-	print(p[0], '的價錢是', p[1])
-
+	print(p[0] + '的價錢是' + p[1])
+	
+#寫入檔案
+with open('menu.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品名稱,價錢\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
 
